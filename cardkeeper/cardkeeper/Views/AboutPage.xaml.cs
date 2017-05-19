ea7554408dfbb5053a568f4f884f8a4437944791
jtpete <jtpete@users.noreply.github.com>
2017-05-18 22:07:22 -0500

    first file setup

---
+++ cardkeeper/cardkeeper/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace cardkeeper.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ cardkeeper/cardkeeper/Views/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cardkeeper.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+            Content = LayoutMainPage();
+        }
+        public static View LayoutMainPage()
+        {
+            var layout = new StackLayout();
+
+
+            var header = new Grid() { ColumnSpacing = 10, RowSpacing = 10 };
+            header.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            header.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            header.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            header.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            header.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            header.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            header.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            header.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+
+            var logo = new Image
+            {
+                Source = "famlogo.png",
+                Aspect = Aspect.Fill
+            };
+            var weather = new Label
+            {
+                Text = "Weather",
+                HorizontalTextAlignment = TextAlignment.End,
+                VerticalTextAlignment = TextAlignment.Center,
+            };
+
+            header.BackgroundColor = Color.FromHex("#AD974F");
+
+
+            header.Children.Add(logo, 0, 0);
+            header.Children.Add(weather, 4, 3);
+            Grid.SetColumnSpan(logo, 3);
+            Grid.SetRowSpan(logo, 4);
+
+            var viewCards = new Button
+            {
+                Text = "VIEW CARDS",
+                FontSize = 30,
+                FontAttributes = FontAttributes.Bold,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                BorderColor = Color.Black,
+                BorderRadius = 5,
+                BorderWidth = 2,
+                BackgroundColor = Color.Blue,
+            };
+            viewCards.Clicked += ViewCardsClicked;
+
+            var addCards = new Button
+            {
+                Text = "ADD CARDS",
+                FontSize = 30,
+                FontAttributes = FontAttributes.Bold,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                BorderColor = Color.Black,
+                BorderRadius = 5,
+                BorderWidth = 2,
+                BackgroundColor = Color.Green
+            };
+            addCards.Clicked += AddCardsClicked;
+
+            layout.Children.Add(header);
+            layout.Children.Add(viewCards);
+            layout.Children.Add(addCards);
+
+
+            return layout;
+        }
+        static void ViewCardsClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Navigation.PushAsync(new NavigationPage(new AboutPage()));
+        }
+        static void AddCardsClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Navigation.PushAsync(new NavigationPage(new ItemsPage()));
+        }
+    }
+}
